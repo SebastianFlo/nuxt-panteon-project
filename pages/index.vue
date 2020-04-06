45e576950aa8cc33fa429f89839da4514e53bd7e
Sebastian Florian <rebardesigndk@gmail.com>
2020-04-06 12:24:08 +0200

    :sparkles: feature(Index): Add description

--- pages/index.vue
+++ pages/index.vue
@@ -70,61 +70,105 @@
         </div>
 
         <div class="columns pan-materials">
-          <card :title="materials.accesorii_mobilier.title" class="is-one-quarter-desktop is-full-mobile" :imageAlt="materials.accesorii_mobilier.title" :image="materials.accesorii_mobilier.image">
+          <card :title="materials.accesorii_mobilier.title" class="is-one-quarter-desktop is-full-mobile"
+                :imageAlt="materials.accesorii_mobilier.title" :image="materials.accesorii_mobilier.image">
           </card>
-          <card :title="materials.accesorii_sarpante.title" class="is-one-quarter-desktop is-full-mobile" :imageAlt="materials.accesorii_sarpante.title" :image="materials.accesorii_sarpante.image">
+          <card :title="materials.accesorii_sarpante.title" class="is-one-quarter-desktop is-full-mobile"
+                :imageAlt="materials.accesorii_sarpante.title" :image="materials.accesorii_sarpante.image">
           </card>
           <!-- <card :title="materials.blaturi_de_bucatarie.title" class="is-one-quarter-desktop is-full-mobile" :imageAlt="materials.blaturi_de_bucatarie.title" :image="materials.blaturi_de_bucatarie.image">
           </card> -->
-          <card :title="materials.bca.title" class="is-one-quarter-desktop is-full-mobile" :imageAlt="materials.bca.title" :image="materials.bca.image">
+          <card :title="materials.bca.title" class="is-one-quarter-desktop is-full-mobile"
+                :imageAlt="materials.bca.title" :image="materials.bca.image">
           </card>
-          <card :title="materials.boltari.title" class="is-one-quarter-desktop is-full-mobile" :imageAlt="materials.boltari.title" :image="materials.boltari.image">
-          </card>
-        </div>
-
-        <div class="columns pan-materials">
-          <card :title="materials.ciment.title" class="is-one-quarter-desktop is-full-mobile" :imageAlt="materials.ciment.title" :image="materials.ciment.image">
-          </card>
-          <card :title="materials.hidroizolatii.title" class="is-one-quarter-desktop is-full-mobile" :imageAlt="materials.hidroizolatii.title" :image="materials.hidroizolatii.image">
-          </card>
-          <card :title="materials.mortar.title" class="is-one-quarter-desktop is-full-mobile" :imageAlt="materials.mortar.title" :image="materials.mortar.image">
-          </card>
-          <card :title="materials.pal.title" class="is-one-quarter-desktop is-full-mobile" :imageAlt="materials.pal.title" :image="materials.pal.image">
+          <card :title="materials.boltari.title" class="is-one-quarter-desktop is-full-mobile"
+                :imageAlt="materials.boltari.title" :image="materials.boltari.image">
           </card>
         </div>
 
         <div class="columns pan-materials">
-          <card :title="materials.panouri_bordurate.title" class="is-one-quarter-desktop is-full-mobile" :imageAlt="materials.panouri_bordurate.title" :image="materials.panouri_bordurate.image">
+          <card :title="materials.ciment.title" class="is-one-quarter-desktop is-full-mobile"
+                :imageAlt="materials.ciment.title" :image="materials.ciment.image">
           </card>
-          <card :title="materials.plasa_gard.title" class="is-one-quarter-desktop is-full-mobile" :imageAlt="materials.plasa_gard.title" :image="materials.plasa_gard.image">
+          <card :title="materials.hidroizolatii.title" class="is-one-quarter-desktop is-full-mobile"
+                :imageAlt="materials.hidroizolatii.title" :image="materials.hidroizolatii.image">
           </card>
-          <card :title="materials.plasa_sudata.title" class="is-one-quarter-desktop is-full-mobile" :imageAlt="materials.plasa_sudata.title" :image="materials.plasa_sudata.image">
+          <card :title="materials.mortar.title" class="is-one-quarter-desktop is-full-mobile"
+                :imageAlt="materials.mortar.title" :image="materials.mortar.image">
           </card>
-          <card :title="materials.rigips.title" class="is-one-quarter-desktop is-full-mobile" :imageAlt="materials.rigips.title" :image="materials.rigips.image">
+          <card :title="materials.pal.title" class="is-one-quarter-desktop is-full-mobile"
+                :imageAlt="materials.pal.title" :image="materials.pal.image">
           </card>
         </div>
 
         <div class="columns pan-materials">
-          <card :title="materials.samota.title" class="is-one-quarter-desktop is-full-mobile" :imageAlt="materials.samota.title" :image="materials.samota.image">
+          <card :title="materials.panouri_bordurate.title" class="is-one-quarter-desktop is-full-mobile"
+                :imageAlt="materials.panouri_bordurate.title" :image="materials.panouri_bordurate.image">
           </card>
-          <card :title="materials.spalieri.title" class="is-one-quarter-desktop is-full-mobile" :imageAlt="materials.spalieri.title" :image="materials.spalieri.image">
+          <card :title="materials.plasa_gard.title" class="is-one-quarter-desktop is-full-mobile"
+                :imageAlt="materials.plasa_gard.title" :image="materials.plasa_gard.image">
           </card>
-          <card :title="materials.termoizolatii.title" class="is-one-quarter-desktop is-full-mobile" :imageAlt="materials.termoizolatii.title" :image="materials.termoizolatii.image">
+          <card :title="materials.plasa_sudata.title" class="is-one-quarter-desktop is-full-mobile"
+                :imageAlt="materials.plasa_sudata.title" :image="materials.plasa_sudata.image">
           </card>
-          <card :title="materials.var.title" class="is-one-quarter-desktop is-full-mobile" :imageAlt="materials.var.title" :image="materials.var.image">
+          <card :title="materials.rigips.title" class="is-one-quarter-desktop is-full-mobile"
+                :imageAlt="materials.rigips.title" :image="materials.rigips.image">
+          </card>
+        </div>
+
+        <div class="columns pan-materials">
+          <card :title="materials.samota.title" class="is-one-quarter-desktop is-full-mobile"
+                :imageAlt="materials.samota.title" :image="materials.samota.image">
+          </card>
+          <card :title="materials.spalieri.title" class="is-one-quarter-desktop is-full-mobile"
+                :imageAlt="materials.spalieri.title" :image="materials.spalieri.image">
+          </card>
+          <card :title="materials.termoizolatii.title" class="is-one-quarter-desktop is-full-mobile"
+                :imageAlt="materials.termoizolatii.title" :image="materials.termoizolatii.image">
+          </card>
+          <card :title="materials.var.title" class="is-one-quarter-desktop is-full-mobile"
+                :imageAlt="materials.var.title" :image="materials.var.image">
           </card>
         </div>
 
       </div>
     </div>
 
+
+    <section class="section columns pan-description">
+      <div class="container column is-8 is-offset-2 has-text-centered">
+        <h3 class="subtitle">Panteon, Baia Mare</h3>
+
+        <p>
+          O afacere localã mică, nu corporație multinațională.
+          <br>
+          <br>
+          De 20 de ani ajutăm maramureșenii să construiască.
+          <br>
+          <br>
+          Prețuri mici și negociabile.
+          <br>
+          <hr>
+          <br>
+          Este un depozit de materiale construcţii in Baia Mare ce ofera:
+          <br>
+          BCA ieftin, accesorii mobilier, ciment, mortal, pal melaminat și multe altele.
+          <br>
+          <hr>
+          <br>
+          Exista și posibilitate de colaborare pentru manufactura de produse.
+
+        </p>
+      </div>
+    </section>
+
     <footer class="footer has-text-white">
       <div class="mb-100 has-text-centered content columns">
         <div class="column is-3 is-offset-2 is-size-3 footer-link">
-          <b-icon icon="phone"/> <a target="blank" :href="phone">0722 682 913</a>
+          <b-icon icon="phone" /> <a target="blank" :href="phone">0722 682 913</a>
         </div>
         <div class="column is-4 is-size-3 footer-link">
-          <b-icon icon="email"/> <a target="blank" :href="email">panteonaspect@gmail.com</a>
+          <b-icon icon="email" /> <a target="blank" :href="email">panteonaspect@gmail.com</a>
         </div>
       </div>
 
@@ -164,7 +208,7 @@
             <tr>
               <th class="has-text-white">sâmbata:</th>
               <td>
-                <span>08:00–00:00</span>
+                <span>08:00–12:00</span>
               </td>
             </tr>
             <tr>
@@ -269,7 +313,6 @@
 </script>
 
 <style>
-
   a {
     color: #d3d3d3;
   }
@@ -298,16 +341,19 @@
     margin-top: 100px;
   }
 
+  .pan-description {
+    background-color: rgba(15, 11, 9, 0.6);
+  }
+
   .footer {
     background-color: #0f0b09;
   }
 
-  .footer > .footer-link {
+  .footer>.footer-link {
     color: white;
   }
 
   .mb-100 {
     margin-bottom: 100px;
   }
-
 </style>
